0caaef1e40244155a2247b6f51c4777852c88963
Natallia Langerbeins <natallia.langerbeins@gmail.com>
2023-06-12 22:46:27 +0300

    added new tasks

--- Week3. Loops/script.js
+++ Week3. Loops/script.js
@@ -124,3 +124,43 @@ const vowelsChecker = (s) => {
 console.log(vowelsChecker('I love to code'))
 
 console.log('I love to code'.match(/[aeiou]/g).length)
+
+
+// TODO: Use a loop to create a number guessing game
+//  1) Ask the user to enter a number and assign that value to the `guess` variable
+//  2) End the loop when the user's guess matches the random number
+//  3) Display a message letting the user know they guessed the number
+
+const main = document.querySelector('main');
+const randomNumber = getRandomNumber(10);
+let guess;
+
+function getRandomNumber(upper) {
+  return Math.floor( Math.random() * upper ) + 1;
+}
+
+while (count < 10) {
+    guess = prompt('I am thinking of a number between 1 and 10. What is it?');
+    count++;
+
+    if (+guess == randomNumber) {
+        main.innerHTML = `It took you ${count} times to guess the number ${randomNumber}`;
+        break;
+    } else {
+        main.innerHTML = `Next time try better.`
+    }
+}
+
+// 
+// for (let i = 0; i < 10; i++) {
+//     guess = prompt('I am thinking of a number between 1 and 10. What is it?');
+//     if (+guess == randomNumber) {
+//       main.innerHTML = `It took you ${i} times to guess the number ${randomNumber}`;
+//       break;
+//     } else {
+//       main.innerHTML = `Next time try better.`
+//     }
+//   }
+  
+
+
